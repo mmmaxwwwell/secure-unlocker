4ad4ee6bef70745a0ccc1395f6fb33e9134ee226
mmmaxwwwell <mmmaxwwwell@protonmail.com>
2026-01-14 21:34:51 -0500

    back to fake hash

--- module.nix
+++ module.nix
@@ -20,7 +20,7 @@ let
 
     # Build once to get correct hash, then replace this placeholder
     # Updated hash after adding express-rate-limit dependency
-    npmDepsHash = "sha256-A0kidXkJucaFKZuLgzYTqGtUffUvcknClbHeSyGouK4=";
+    npmDepsHash = lib.fakeHash;
 
     buildPhase = ''
       runHook preBuild
